0be9178e87bfaa48223a0eaf3b46f05b30d76a35
John <ekapob.t@ku.th>
2022-03-28 18:31:02 +0700

    UI panel

---
+++ UnityProject/RectTool/Assets/Scripts/UI/Menu/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : Button
+{
+    [SerializeField] private GameObject SubmenuGameObject;
+
+    public void Hide() => SubmenuGameObject?.SetActive(false);
+
+    public void Show() => SubmenuGameObject?.SetActive(true);
+}
---
+++ UnityProject/RectTool/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private static UIController _instance;
+    public static UIController Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<UIController>();
+
+            return _instance;
+        }
+    }
+
+    [SerializeField] private Button outAreaButton;
+
+    [Header("Menu Button")]
+    [SerializeField] private MenuFile fileButton;
+    [SerializeField] private MenuImageList ImageListButton;
+
+    public void Awake()
+    {
+        fileButton.onClick.RemoveAllListeners();
+        fileButton.onClick.AddListener(OnFileClick);
+        ImageListButton.onClick.RemoveAllListeners();
+        ImageListButton.onClick.AddListener(OnImageListClick);
+    }
+
+    public void OnFileClick()
+    {
+        // Show File Menu new load set bg
+    }
+
+    public void OnImageListClick()
+    {
+        // Show Side Image
+    }
+}
